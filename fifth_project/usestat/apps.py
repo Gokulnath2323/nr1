90282c20b2149e646c3eac0f6d82c8405d5d5ad4
Gokulnath2323 <110990662+Gokulnath2323@users.noreply.github.com>
2022-11-29 05:18:28 -0800

    Delete fifth_project directory

--- fifth_project/usestat/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsestatConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'usestat'
--- fifth_project/usestat/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-def res(request):
-     return render(request,'ap1/index.html')
-
-
-# Create your views here.
